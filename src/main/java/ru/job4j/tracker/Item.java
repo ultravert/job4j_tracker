b3b8ff09eaa3b04e3d11795bee7f79bd273ec3ac
ultravert <feodor.sax@gmail.com>
2023-01-27 21:27:34 +0500

    10. toString [#310018 #385626]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,7 +7,7 @@ public class Item {
 
     private int id;
     private String name;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
@@ -20,11 +20,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-        this.created = getCreated();
     }
 
     public LocalDateTime getCreated() {
-        return LocalDateTime.now();
+        return created;
     }
 
     public int getId() {
