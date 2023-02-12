faaeaa44acc815931f294c554f64b6669d190a99
ultravert <feodor.sax@gmail.com>
2023-02-12 17:52:10 +0500

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -45,8 +45,8 @@ public class Item {
     @Override
     public String toString() {
         return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER) + '}';
+                + "id = " + id
+                + ", name = '" + name + '\''
+                + ", created = " + created.format(FORMATTER) + '}';
     }
 }
