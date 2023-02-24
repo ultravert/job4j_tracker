ec213dfb4e40451945fde575aaf9af8c896aeb52
ultravert <feodor.sax@gmail.com>
2023-02-24 18:24:36 +0500

    9. Тестирование. Подготовка данных. [#182960 #394704]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -39,6 +39,6 @@ public class StartUITest {
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted).isEqualTo(null);
+        assertThat(deleted).isNull();
     }
 }
