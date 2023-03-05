1045a28b03f8499db9d4f3b08a18a92e75706a7e
ultravert <feodor.sax@gmail.com>
2023-03-05 13:35:33 +0500

    0.4. Пользовательские исключения. [#219367 #397447]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,7 +2,7 @@ package ru.job4j.ex;
 
 import java.util.Objects;
 
-public class FindEl extends Exception {
+public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
@@ -21,7 +21,7 @@ public class FindEl extends Exception {
         String[] elements = {"Ferrari", "Porsche", "Maserati", "Aston Martin"};
         String el = "Ford";
         try {
-            new FindEl().indexOf(elements, el);
+            indexOf(elements, el);
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
