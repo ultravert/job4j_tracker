77267d17f1fa7976b3ba259e902afbab54ae2729
ultravert <feodor.sax@gmail.com>
2023-07-30 18:07:54 +0500

    2. Идентичные тексты [#212655 # [#212655]]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String string : text) {
+            if (!check.contains(string)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
